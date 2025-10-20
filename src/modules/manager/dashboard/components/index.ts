251a807b2816c7c08451d7efa0ffc62606ce014e
vietdung <vovietdung20013007@gmail.com>
2025-10-21 03:22:00 +0700

    -- Dung FE -- update UI dashboard (none API)

---
+++ src/modules/manager/dashboard/components/index.ts
@@ -0,0 +1,8 @@
+// Dashboard Components
+export { default as DashboardPage } from './dashboard/page'
+export { StatsCards } from './StatsCards'
+export { QueueMonitoring } from './QueueMonitoring'
+export { ChartsSection } from './ChartsSection'
+export { RecentActivities } from './RecentActivities'
+export { QuickActions } from './QuickActions'
+export { SystemAlerts } from './SystemAlerts'
