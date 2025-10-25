413aabd3a41d80d7e16c8188d93346eb5afd94c5
Hung Ngo <nghung179123@gmail.com>
2025-10-25 23:31:48 +0700

    fix

--- src/modules/staff/dashboard/components/ui/case/detail/PaymentConfirmButton.ui.tsx
+++ src/modules/staff/dashboard/components/ui/case/detail/PaymentConfirmButton.ui.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
-import { BaseModal } from '@/shared/components/manager/modal/BaseModal';
+import { BaseModal } from '@/shared/components/layout/manager/modal/BaseModal';
 import { useConfirmPayment } from '../../../../hooks/useConfirmPayment';
 
 interface PaymentConfirmButtonProps {
