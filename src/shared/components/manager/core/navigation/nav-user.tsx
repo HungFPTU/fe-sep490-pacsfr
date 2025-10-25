bfa12dd41457269d194c8292e3a3c442c5ad9667
server-craftsman <huyit2003@gmail.com>
2025-10-25 13:39:16 +0700

    chore: manager sidebar

--- src/shared/components/manager/core/navigation/nav-user.tsx
+++ src/shared/components/manager/core/navigation/nav-user.tsx
@@ -33,7 +33,7 @@ export function NavUser({
   user,
 }: {
   user: {
-    name: string
+    fullName: string
     email: string
     avatar: string
   }
@@ -55,13 +55,13 @@ export function NavUser({
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user.avatar} alt={user.name} />
+                <AvatarImage src={user.avatar} alt={user.fullName} />
                 <AvatarFallback className="rounded-lg bg-purple-100 text-purple-600">
-                  {user.name.charAt(0).toUpperCase()}
+                  {user.fullName.charAt(0).toUpperCase()}
                 </AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-semibold">{user.name}</span>
+                <span className="truncate font-semibold">{user.fullName}</span>
                 <span className="truncate text-xs">{user.email}</span>
               </div>
               <ChevronsUpDown className="ml-auto size-4" />
@@ -76,13 +76,13 @@ export function NavUser({
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
+                  <AvatarImage src={user.avatar} alt={user.fullName} />
                   <AvatarFallback className="rounded-lg bg-purple-100 text-purple-600">
-                    {user.name.charAt(0).toUpperCase()}
+                    {user.fullName.charAt(0).toUpperCase()}
                   </AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-semibold">{user.name}</span>
+                  <span className="truncate font-semibold">{user.fullName}</span>
                   <span className="truncate text-xs">{user.email}</span>
                 </div>
               </div>
