c63fb011ef670f9abad385643e1048adee29b805
Nguyễn Đan Huy <huyit2003@gmail.com>
2025-11-12 14:17:46 +0700

    refactor: staff layout, main layout

--- src/modules/staff/dashboard/components/view/StaffDashboardTabsView.view.tsx
+++ src/modules/staff/dashboard/components/view/StaffDashboardTabsView.view.tsx
@@ -8,7 +8,7 @@ interface StaffDashboardTabsViewProps {
 
 export const StaffDashboardTabsView: React.FC<StaffDashboardTabsViewProps> = ({ children }) => {
   return (
-    <div className="bg-gray-50 py-8">
+    <div className="min-h-screen py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {children}
       </div>
